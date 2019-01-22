375d3e5161acefa752c803f6ace40981f110f33c
Miguel Sanchez Rico <migueli.sanchez.rico@gmail.com>
2019-01-22 19:48:59 +0100

    adding cpp

---
+++ src/cv_bridge_demo.cpp
@@ -0,0 +1,9 @@
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "cv_bridge_demo");
+  ros::NodeHandle nh;
+
+  ROS_INFO("Hello world, So this Is the right one!");
+}
